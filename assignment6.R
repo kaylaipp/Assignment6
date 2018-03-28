8be27e2ecc66b6646386e163af0c604853df859a
kaylaipp <kaylaipp@bu.edu>
2018-03-28 19:02:46 -0400

    changed pdf output so graph output is dynamic and changes with code rather than static screenshots

--- assignment6.R
+++ assignment6.R
@@ -7,7 +7,7 @@ bude <- geocode("Bude, England")
 p1 <- geocode("Bude and North Cornwall Golf Club")
 p2 <- geocode("Bude Tidal Swimming Pool")
 p3 <- geocode("The Carrier's Inn Pub, Bude England")
-p4 <- geocode("Compass Point, Bude England")
+p4 <- geocode("Compass Point, Bude England") #
 
 #by Deyan
 beach <- geocode("Summerleaze Beach") 
@@ -26,27 +26,25 @@ path1 <- data.frame("longs" = c(longs[2],longs[4]), "lats" = c(lats[2],lats[4]))
 print(path1)
 
 
-#regular map
-png('regmap.png')
-map <-get_googlemap(center = c(lon=-4.550,lat=50.83),markers = points12,path=path1,zoom = 15)
-ggmap(map)
-map2<-ggmap(map)+
+#regular regmap
+regmap <-get_googlemap(center = c(lon=-4.550,lat=50.83),markers = points12,path=path1,zoom = 15)
+ggmap(regmap)
+map2<-ggmap(regmap)+
   geom_point(
     aes(x=longs,y=lats),
     data=points12, size = 1)
 plot(map2)
-dev.off()
 
-#water color map 
-png('watercolor-map.png')
-map3 <-get_map(location = c(-4.550,50.83),source = "google",zoom = 14,maptype = "watercolor")
-ggmap(map3)
-map4<-ggmap(map3)+
+
+#water color gmap 
+watercolor_map <-get_map(location = c(-4.550,50.83),source = "google",zoom = 14,maptype = "watercolor")
+ggmap(watercolor_map)
+map4<-ggmap(watercolor_map)+
   geom_point(
     aes(x=longs,y=lats),
     data=points12, size = 4)
 plot(map4)
-dev.off()
+
 
 #by Deyan
 SummerleazeBeach <- image_scale(image_read('https://upload.wikimedia.org/wikipedia/commons/9/99/Beach_huts_on_Summerleaze_Beach.jpg'))
